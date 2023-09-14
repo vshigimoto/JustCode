f6f2ad3a0b996391625ea7af17a7b7bfd46bda86
vshigimoto <shigimoto@mail.ru>
2023-09-14 22:43:41 +0600

    task2

---
+++ Lecture01/task2/main.go
@@ -0,0 +1,9 @@
+func longestCommonPrefix(strs []string) string {
+	for i := 0; ; i++ {
+		for _, str := range strs {
+			if i == len(str) || str[i] != strs[0][i] {
+				return strs[0][:i]
+			}
+		}
+	}
+}
