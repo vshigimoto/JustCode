c17e2786459cdd7eecee7240f900494f27cbea18
vshigimoto <shigimoto@mail.ru>
2023-09-14 22:32:03 +0600

    task #1

--- Lecture01/task1/main.go
+++ Lecture01/task1/main.go
@@ -1,7 +1,3 @@
-package main
-
-import "fmt"
-
 func twoSum(nums []int, target int) []int {
 	nmap := make(map[int]int)
 	for i := 0; i < len(nums); i++ {
@@ -14,10 +10,4 @@ func twoSum(nums []int, target int) []int {
 		nmap[nums[i]] = i
 	}
 	return []int{}
-}
-
-func main() {
-	nums := []int{2, 7, 11, 15}
-	target := 9
-	fmt.Println(twoSum(nums, target))
 }
