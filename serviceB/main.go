3919bb121ada609805a07e808750b1c8912b0eae
Amirkhan <96425010+vshigimoto@users.noreply.github.com>
2023-10-10 23:00:27 +0600

    Update main.go

--- serviceB/main.go
+++ serviceB/main.go
@@ -11,16 +11,16 @@ func main() {
 		// Take information from service A
 		body, err := ioutil.ReadAll(r.Body)
 		if err != nil {
-			fmt.Println("Ошибка при чтении данных от сервиса A:", err)
-			http.Error(w, "Ошибка при чтении данных от сервиса A", http.StatusBadRequest)
+			fmt.Println("Error request A:", err)
+			http.Error(w, "Error request A", http.StatusBadRequest)
 			return
 		}
 
 		// data from serviceA
-		fmt.Println("Данные от сервиса A:", string(body))
+		fmt.Println("Data from service A:", string(body))
 
 		// response to serviceA
-		fmt.Fprintln(w, "Ответ от сервиса B")
+		fmt.Fprintln(w, "Data from service B")
 	})
 
 	// Start
