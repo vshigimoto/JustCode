3062d9af6aaa146b248a5b43e88ee16da5e4ba03
Amirkhan Mubarakov <96425010+vshigimoto@users.noreply.github.com>
2023-10-21 22:47:03 +0600

    Add files via upload

---
+++ booking/cmd/redis/main.go
@@ -0,0 +1,16 @@
+package main
+
+//
+//import "github.com/gin-gonic/gin"
+//
+//func main() {
+//	r := gin.Default()
+//	v1 := r.Group("v1")
+//	{
+//		v1.GET("value", GetValue())
+//	}
+//	err := r.Run(":8080")
+//	if err != nil {
+//		return
+//	}
+//}
